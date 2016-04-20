b3b60a0f4b50cb4cacea89d628723a643f222a2f
Tyler Menezes <tylermenezes@gmail.com>
2016-04-20 14:24:11 -0700

    Remove blog

--- public/build/wp-content/themes/studentrnd-shim/404.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (!class_exists('Timber')){
-    echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-}
-
-$context = Timber::get_context();
-
-Timber::render('errors/404.twig', $context);
