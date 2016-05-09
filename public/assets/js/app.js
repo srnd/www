6fe8e4f4a7dd4545a7b2c3016bd6aff6735b9438
Tyler Menezes <tylermenezes@gmail.com>
2016-05-08 22:22:53 -0700

    Remove GA

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -238,17 +238,6 @@ var allRegions = [
 
 
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-8868191-12', 'auto');
-ga('send', 'pageview');
-
-InstantClick.on('change', function(){
-    ga('send', 'pageview');
-});
 InstantClick.on('change', function(){
     $('.pie').sparkline('html', {type: "pie", sliceColors: ['#484848', '#DFDFDF'], offset: -90, disableInteraction: true});
 });
