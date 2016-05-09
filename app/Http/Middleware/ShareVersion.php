39e94a45e1ef9d3bef5d1acb2ec11bd546590488
Tyler Menezes <tylermenezes@gmail.com>
2016-05-08 21:50:22 -0700

    Add version tracking, css minify

---
+++ app/Http/Middleware/ShareVersion.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace StudentRND\Http\Middleware;
+
+class ShareVersion
+{
+    public function handle($request, \Closure $next)
+    {
+        \View::share('version', trim(@file_get_contents(dirname(dirname(dirname(dirname(__FILE__)))).'/.version')));
+        return $next($request);
+    }
+}
