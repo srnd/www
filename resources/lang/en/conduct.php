1cb3698b34649965bc9f91d853e43acd744cacac
Tyler Menezes <tylermenezes@gmail.com>
2017-06-07 11:48:21 -0700

    Add conduct to translations

---
+++ resources/lang/en/conduct.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+    "title"                 => "Code of Conduct",
+    "intro"                 => "srnd.org strives to make students feel welcome so they can meet new friends, have fun, and build cool things. To create this atmosphere, we need the help of the whole community.",
+    "friendly-header"       => "At srnd.org events, please be friendly and welcoming.",
+    "friendly-humble"       => "Be humble, even when you're successful.",
+    "friendly-respect"      => "Respect, encourage, and support others.",
+    "friendly-language"     => "Don't use offensive language referring to people's gender, race, sexual orientation, religion, or disabilities. Don't use sexualized language or imagery.",
+    "friendly-hit-on"       => "Don't &ldquo;hit on&rdquo; other attendees, even if you think the person is receptive.",
+    "harass-header"         => "srnd.org members may never harass others.",
+    "harass-text"           => "Harassment includes words or behavior which are intended to disparage, demean, mock, or intimidate a person or group. People who participate in harassment are not welcome at srnd.org events, even when this behavior takes place outside the event.",
+    "legal-header"          => "At srnd.org events, keep things safe and legal.",
+    "legal-safety"          => "Ensure the safety of yourself and others. Do not carry guns, knives, or other weapons. Deescalate conflicts when possible; get staff help when not.",
+    "legal-drugs"           => "Abstain from using or being under the influence of alcohol, marijuana, illicit substances, and recreational drugs.",
+    "enforcement-header"    => "Enforcement",
+    "enforcement-report"    => "If you notice something which you think violates this code, please report it, even if it didn't happen to you. Anyone who reports a suspected violation in good faith will receive free admission to a future event.",
+    "enforcement-process"   => "All reports will be treated as anonymously as possible, and will initially go directly to global management. You will not experience any retaliation from volunteers, mentors, or staff. To make a report:",
+    "enforcement-email"     => "email",
+    "enforcement-phone"     => "phone",
+    "enforcement-slack"     => "slack",
+    "sanctions" => "Community members violating these rules will be sanctioned at the discretion of srnd.org staff, including an expulsion or ban from all srnd.org activities without refund.",
+    "last-updated"          => "Last updated :date",
+);
