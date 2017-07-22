165a5d0f78b370b1a75bc5c2ae2ae57d99939b86
Tyler Menezes <tylermenezes@gmail.com>
2017-07-22 10:33:01 -0700

    Add talkus

--- resources/assets/scripts/src/index.js
+++ resources/assets/scripts/src/index.js
@@ -1,3 +1,14 @@
 import DonationPage from './donate'
 
-DonationPage()
+if (document.body.classList.contains('section-donate')) {
+    DonationPage();
+}
+
+document.querySelector('footer input.ein')
+    .addEventListener('click', (evt) => {
+        evt.target.select();
+        document.execCommand('copy');
+    });
+
+(function(t,a,l,k,u,s,e){if(!t[u]){t[u]=function(){(t[u].q=t[u].q||[]).push(arguments)},t[u].l=1*new Date();s=a.createElement(l),e=a.getElementsByTagName(l)[0];s.async=1;s.src=k;e.parentNode.insertBefore(s,e)}})(window,document,'script','//www.talkus.io/plugin.beta.js','talkus');
+talkus('init', 'DptgxkJDNvbkLB2mX');
