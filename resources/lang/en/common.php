ef55e9a01cb15cf0ca2ee5ea4d21f70612ade3ee
Tyler Menezes <tylermenezes@gmail.com>
2017-08-08 10:54:47 -0700

    Update language

--- resources/lang/en/common.php
+++ resources/lang/en/common.php
@@ -2,7 +2,7 @@
 
 return array(
     "name"              => "srnd.org",
-    "tagline"           => "Increasing Computer Science enrollment nationwide.",
+    "tagline"           => "Increasing diverse Computer Science enrollment.",
     "tagline-title"     => "tens of thousands of coders created",
     "desc"              => "srnd.org works with hundreds of schools and districts to increase CS enrollment nationwide. Since 2012 we've created tens of thousands of new coders.",
     "desc-social"       => "srnd.org works with hundreds of schools and districts to increase CS enrollment nationwide. Since 2012 we've created tens of thousands of new coders.",
--- resources/lang/es/common.php
+++ resources/lang/es/common.php
@@ -2,7 +2,7 @@
 
 return array(
     "name"              => "srnd.org",
-    "tagline"           => "Aumentando la matrícula en computación a lo largo de todo el país.",
+    "tagline"           => "Aumentando la diversa matrícula en computación a lo largo de todo el país.",
     "tagline-title"     => "decenas de miles de programadores formados",
     "desc"              => "Srnd.org trabaja con cientos de escuelas y distritos para aumentar las inscripciones a cursos de programación a lo largo de todo el país. Desde el 2012 hemos formado decenas de miles de nuevos programadores.",
     "desc-social"       => "Srnd.org trabaja con cientos de escuelas y distritos para aumentar las inscripciones a cursos de programación a lo largo de todo el país. Desde el 2012 hemos formado decenas de miles de nuevos programadores.",
