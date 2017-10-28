1b44d640711e49a85ec8238f2a4dcffeeeda519d
Tyler Menezes <tylermenezes@gmail.com>
2017-10-27 19:01:38 -0700

    Update images

--- resources/lang/en/contact.php
+++ resources/lang/en/contact.php
@@ -24,6 +24,6 @@ return array(
     "team-bio-wiedmann"         => "Zaq is one of srnd.org's earliest volunteers, helping to develop much of the early pedagogy. He works as an Engineer at Tune.",
     "team-bio-crimmins"         => "Bob is a seasoned entrepreneur, an advisor to Startup Weekend, and a mentor at Techstars with a background in engineering.",
     "team-bio-pooley"           => "Ana Pooley is a CodeDay alum, a student at Macalester College, and a recipient of the NCWIT Aspirations in Computing Award.",
-    "team-bio-jiang"            => "The original founder of StudentRND, Edward served as its Executive Director until early 2014. He is currently a contractor at Ship It.",
+    "team-bio-jiang"            => "The original founder of StudentRND, Edward served as its Executive Director until early 2014. He currently works at Uber.",
     "team-bio-kindel"           => "Charlie is the GM of Amazon Alexa Smart Home, and previously led Windows Phone. He served a full board term from 2013-2017.",
 );
