495e459310ba4ced530a25e011349ba76b238fd7
Tyler Menezes <tylermenezes@gmail.com>
2018-06-04 13:29:41 -0700

    Add spinner for forms

--- src/components/Blocks/SchemaForm/index.js
+++ src/components/Blocks/SchemaForm/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { Pii } from '../../Ui/Secure'
+import Loading from '../../Ui/Loading'
 import Form from 'react-jsonschema-form'
 import WithIpInfo from '../../Track/ipInfo'
 
@@ -9,34 +10,37 @@ import "./index.sass"
 class SchemaForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {submitted: false};
+        this.state = {submitted: false, submitting: false};
     }
     render() {
         return (
             <div className="schema-form">
                 {this.state.submitted
                         ? <p className="submitted">{this.props.submitMessage}</p>
-                        : <Form
-                            schema={JSON.parse(this.props.schema.internal.content)}
-                            uiSchema={JSON.parse(this.props.uiSchema.internal.content)}
-                            onSubmit={(e) => this.onSubmit(e.formData)}>
-                                <div className="bottom">
-                                    <div className="submit">
-                                        <button type="submit">{this.props.cta}</button>
+                        : (this.state.submitting ? <Loading /> : 
+                            <Form
+                                schema={JSON.parse(this.props.schema.internal.content)}
+                                uiSchema={JSON.parse(this.props.uiSchema.internal.content)}
+                                onSubmit={(e) => this.onSubmit(e.formData)}>
+                                    <div className="bottom">
+                                        <div className="submit">
+                                            <button type="submit">{this.props.cta}</button>
+                                        </div>
+                                        <Pii />
                                     </div>
-                                    <Pii />
-                                </div>
-                            </Form>
+                                </Form>
+                        )
                 }
             </div>
         );
     }
 
     onSubmit(formData) {
+        this.setState({submitting: true});
         axios
             .get(this.props.submitUrl, {params: Object.assign(formData, {visitor: this.props.ipInfo})})
             .then((response) => this.setState({submitted: true}))
-            .catch((error) => alert(error));
+            .catch((error) => {alert(error); this.setState({submitting: false});});
     }
 }
 export default WithIpInfo(SchemaForm);
