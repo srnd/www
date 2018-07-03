b57d49e5c258b1b2a228e443159d771821949a14
Tyler Menezes <tylermenezes@gmail.com>
2018-07-03 15:51:39 -0700

    Animate map

--- src/components/Fragments/ProgramMap/index.js
+++ src/components/Fragments/ProgramMap/index.js
@@ -6,6 +6,7 @@ import { withPrefix } from 'gatsby-link'
 import axios from 'axios'
 import ReactTooltip from 'react-tooltip'
 import withIpInfo from '../../Track/ipInfo'
+import TrackVisibility from 'react-on-screen'
 
 import './index.sass'
 
@@ -33,116 +34,118 @@ class ProgramMap extends React.Component {
         const sizeDivisor = 100;
 
         return (
-            <div className="program-map" ref={(el) => {
-                this.renderTarget = el;
-                if (!(this.state.width && this.state.height)) { this.updateDimensions() } 
-            }}>
-                {!(this.state.width && this.state.height) ? null : (
-                    <svg width={this.state.width} height={this.state.height} viewBox={`0 0 ${this.state.width} ${this.state.height}`}>
-                        <defs>
-                            <clipPath id="clip-land">
-                                <path
-                                    d={
-                                        this.state.topo.map((d,i) => (
-                                            geoPath().projection(this.projection())(d)
-                                        )).join('')} />
-                            </clipPath>
-                        </defs>
-                        <g className="outlines">
-                            {
-                                this.state.topo.map((d,i) => (
-                                <path
-                                    key={ `path-${ i }` }
-                                    d={ geoPath().projection(this.projection())(d) }
-                                    className="political-boundry"
+            <TrackVisibility once>{({ isVisible }) => (
+                <div className={`program-map ${isVisible ? 'seen' : ''}`} ref={(el) => {
+                    this.renderTarget = el;
+                    if (!(this.state.width && this.state.height)) { this.updateDimensions() } 
+                }}>
+                    {!(this.state.width && this.state.height) ? null : (
+                        <svg width={this.state.width} height={this.state.height} viewBox={`0 0 ${this.state.width} ${this.state.height}`}>
+                            <defs>
+                                <clipPath id="clip-land">
+                                    <path
+                                        d={
+                                            this.state.topo.map((d,i) => (
+                                                geoPath().projection(this.projection())(d)
+                                            )).join('')} />
+                                </clipPath>
+                            </defs>
+                            <g className="outlines">
+                                {
+                                    this.state.topo.map((d,i) => (
+                                    <path
+                                        key={ `path-${ i }` }
+                                        d={ geoPath().projection(this.projection())(d) }
+                                        className="political-boundry"
+                                    />
+                                    ))
+                                }
+                            </g>
+                            <g className="footprint-extended">
+                                    {
+                                        this.props.context.regions.map((region, i) => {
+                                            if (!region.location) return null;
+                                            const latLon = [region.location.lon, region.location.lat];
+                                            const mapLatLon = this.projection()(latLon);
+                                            return <circle
+                                                key={ `footprint-extended-${i}` }
+                                                cx={ mapLatLon[0] }
+                                                cy={ mapLatLon[1] }
+                                                r={ (this.state.width/sizeDivisor)*6 }
+                                                clipPath="url(#clip-land)"
+                                                className="footprint-extended"
+                                                onClick={ () => null }
+                                            />
+                                        })
+                                    }
+                            </g>
+                            <g className="footprint">
+                                    {
+                                        this.props.context.regions.map((region, i) => {
+                                            if (!region.location) return null;
+                                            const latLon = [region.location.lon, region.location.lat];
+                                            const mapLatLon = this.projection()(latLon);
+                                            return <circle
+                                                key={ `footprint-${i}` }
+                                                cx={ mapLatLon[0] }
+                                                cy={ mapLatLon[1] }
+                                                r={ (this.state.width/sizeDivisor)*4 }
+                                                clipPath="url(#clip-land)"
+                                                className="footprint"
+                                                onClick={ () => null }
+                                            />
+                                        })
+                                    }
+                            </g>
+                            <g className="region">
+                                    {
+                                        this.props.context.regions.map((region, i) => {
+                                            if (!region.location) return null;
+                                            const latLon = [region.location.lon, region.location.lat];
+                                            const mapLatLon = this.projection()(latLon);
+                                            return <circle
+                                                key={ `region-${i}` }
+                                                cx={ mapLatLon[0] }
+                                                cy={ mapLatLon[1] }
+                                                r={ (this.state.width/sizeDivisor)*2 }
+                                                clipPath="url(#clip-land)"
+                                                className="region"
+                                                onClick={ () => null }
+                                            />
+                                        })
+                                    }
+                            </g>
+                            <g className="marker">
+                                    {
+                                        this.props.context.regions.map((region, i) => {
+                                            if (!region.location) return null;
+                                            const latLon = [region.location.lon, region.location.lat];
+                                            const mapLatLon = this.projection()(latLon);
+                                            return <circle
+                                                data-tip={region.name}
+                                                key={ `marker-${i}` }
+                                                cx={ mapLatLon[0] }
+                                                cy={ mapLatLon[1] }
+                                                r={ this.state.width/sizeDivisor }
+                                                className="marker"
+                                                onClick={ () => null }
+                                            />
+                                        })
+                                    }
+                            </g>
+                            <g className="user">
+                                <circle
+                                    className="user"
+                                    cx={this.projection()([this.props.ipInfo.lon, this.props.ipInfo.lat])[0]}
+                                    cy={this.projection()([this.props.ipInfo.lon, this.props.ipInfo.lat])[1]}
+                                    r={ this.state.width/sizeDivisor }
+                                    onClick={ () => null }
                                 />
-                                ))
-                            }
-                        </g>
-                        <g className="footprint-extended">
-                                {
-                                    this.props.context.regions.map((region, i) => {
-                                        if (!region.location) return null;
-                                        const latLon = [region.location.lon, region.location.lat];
-                                        const mapLatLon = this.projection()(latLon);
-                                        return <circle
-                                            key={ `footprint-extended-${i}` }
-                                            cx={ mapLatLon[0] }
-                                            cy={ mapLatLon[1] }
-                                            r={ (this.state.width/sizeDivisor)*6 }
-                                            clipPath="url(#clip-land)"
-                                            className="footprint-extended"
-                                            onClick={ () => null }
-                                        />
-                                    })
-                                }
-                        </g>
-                        <g className="footprint">
-                                {
-                                    this.props.context.regions.map((region, i) => {
-                                        if (!region.location) return null;
-                                        const latLon = [region.location.lon, region.location.lat];
-                                        const mapLatLon = this.projection()(latLon);
-                                        return <circle
-                                            key={ `footprint-${i}` }
-                                            cx={ mapLatLon[0] }
-                                            cy={ mapLatLon[1] }
-                                            r={ (this.state.width/sizeDivisor)*4 }
-                                            clipPath="url(#clip-land)"
-                                            className="footprint"
-                                            onClick={ () => null }
-                                        />
-                                    })
-                                }
-                        </g>
-                        <g className="region">
-                                {
-                                    this.props.context.regions.map((region, i) => {
-                                        if (!region.location) return null;
-                                        const latLon = [region.location.lon, region.location.lat];
-                                        const mapLatLon = this.projection()(latLon);
-                                        return <circle
-                                            key={ `region-${i}` }
-                                            cx={ mapLatLon[0] }
-                                            cy={ mapLatLon[1] }
-                                            r={ (this.state.width/sizeDivisor)*2 }
-                                            clipPath="url(#clip-land)"
-                                            className="region"
-                                            onClick={ () => null }
-                                        />
-                                    })
-                                }
-                        </g>
-                        <g className="marker">
-                                {
-                                    this.props.context.regions.map((region, i) => {
-                                        if (!region.location) return null;
-                                        const latLon = [region.location.lon, region.location.lat];
-                                        const mapLatLon = this.projection()(latLon);
-                                        return <circle
-                                            data-tip={region.name}
-                                            key={ `marker-${i}` }
-                                            cx={ mapLatLon[0] }
-                                            cy={ mapLatLon[1] }
-                                            r={ this.state.width/sizeDivisor }
-                                            className="marker"
-                                            onClick={ () => null }
-                                        />
-                                    })
-                                }
-                        </g>
-                        <g className="user">
-                            <circle
-                                className="user"
-                                cx={this.projection()([this.props.ipInfo.lon, this.props.ipInfo.lat])[0]}
-                                cy={this.projection()([this.props.ipInfo.lon, this.props.ipInfo.lat])[1]}
-                                r={ this.state.width/sizeDivisor }
-                                onClick={ () => null }
-                            />
-                        </g>
-                    </svg>
-                )}
-            </div>
+                            </g>
+                        </svg>
+                    )}
+                </div>
+            )}</TrackVisibility>
         );
     }
 
