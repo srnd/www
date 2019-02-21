60b7caf59f4708ae812fdfc1a587300d313ebb8a
Tyler Menezes <tylermenezes@gmail.com>
2019-02-21 15:49:51 -0800

    Update styling

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 rm -rf .cache public
 gatsby build
 
+if [ $? -eq 0 ]; then
+    echo OK
+else
+    exit -1
+fi
+
 pushd public
 find . -type f -print0 | xargs -0 sed -i 's/images\.ctfassets\.net/f2\.srnd\.org/g'
 popd
